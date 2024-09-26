c60c26ae7042eb40917f824134c97fc9f6890251
Anthony Suarez <asuarezop@gmail.com>
2024-09-26 10:04:08 -0400

    Added new files to ShoppingList

---
+++ workbook-1/ShoppingList/src/main/java/com/pluralsight/shoppinglist/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.pluralsight.shoppinglist;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Eggs");
+        System.out.println("Bread");
+        System.out.println("Milk");
+        System.out.println("Chips");
+        System.out.println("Bananas");
+        System.out.println("Apples");
+        System.out.println("Waffles");
+        System.out.println("Cereal");
+        System.out.println("Oatmeal");
+        System.out.println("Tea");
+    }
+}
