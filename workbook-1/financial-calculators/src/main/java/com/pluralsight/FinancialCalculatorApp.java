c501e560ac5f8e26220cd48e27deb317db3c4597
Anthony Suarez <asuarezop@gmail.com>
2024-10-02 10:35:18 -0400

    Added functionality for user to change calculators inside FinancialCalculatorApp.java

--- workbook-1/financial-calculators/src/main/java/com/pluralsight/FinancialCalculatorApp.java
+++ workbook-1/financial-calculators/src/main/java/com/pluralsight/FinancialCalculatorApp.java
@@ -1,15 +1,23 @@
 package com.pluralsight;
+import java.util.Scanner;
 
 public class FinancialCalculatorApp {
     public static void main(String[] args) {
-        int calcSelection = 0;
+        Scanner inputSc = new Scanner(System.in);
 
-        System.out.println("Please enter desired ");
+        System.out.print("Welcome! Please select desired calculator to continue. \n");
+        System.out.print("1: Mortgage Calculator\n2: Future Value Calculator\n3: Annuity Calculator\n\n");
+        System.out.println("Enter your selection here: ");
 
+        int calcSelection = inputSc.nextInt();
+
+        //Changing calculators based on user input
         if (calcSelection == 1) {
             MortgageCalculator.main(args);
         } else if (calcSelection == 2) {
             FutureValueCalculator.main(args);
+        } else if (calcSelection == 3){
+            AnnuityCalculator.main(args);
         }
     }
 }
